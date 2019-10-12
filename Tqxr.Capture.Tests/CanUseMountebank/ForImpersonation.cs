859198f7828465b54d2e4e3c89bad735a7589cce
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2019-10-13 00:31:29 +1100

    capture-api: can launch an HTTP client and talk to Mountebank

--- Tqxr.Capture.Tests/CanUseMountebank/ForImpersonation.cs
+++ Tqxr.Capture.Tests/CanUseMountebank/ForImpersonation.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using FluentAssertions;
 using Tqxr.Capture.Lib;
 using Tqxr.Capture.Lib.OperatingModel;
@@ -14,6 +15,14 @@ namespace Tqxr.Capture.Tests.CanUseMountebank
                 CaptureApi.ControlledApi(typeof(StartupDevelopment).Assembly.FullName);
 
             StartupDevelopment.TestConfiguration.MountebankUrl.Should().Be("http://localhost:2525");
+
+            string hello = new HttpClient().GetStringAsync("http://localhost:2525/imposters/").Result;
+
+            string expected = @"{
+  ""imposters"": []
+}";
+
+            hello.Should().Be(expected);
         }
     }
 }
