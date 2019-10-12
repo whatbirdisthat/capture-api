d0625066ffbb05a868157661f03c9fffb5235544
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2019-10-12 14:00:31 +1100

    build: targeting netcoreapp3.0

--- Tqxr.Capture.Tests/CanUseMountebank/ForImpersonation.cs
+++ Tqxr.Capture.Tests/CanUseMountebank/ForImpersonation.cs
@@ -1,7 +1,34 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Xunit;
+
 namespace Tqxr.Capture.Tests.CanUseMountebank
 {
     public class ForImpersonation
     {
-        
+        [Fact]
+        public void ToCreateANewImposter()
+        {
+            IWebHostBuilder forFakeWebHost = WebHost.CreateDefaultBuilder();
+            forFakeWebHost
+                .UseEnvironment("development")
+                .UseStartup(typeof(ServerAPI))
+                .ConfigureServices(services => { });
+            
+        }
+    }
+
+    public class ServerAPI : IStartup
+    {
+        public void Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
